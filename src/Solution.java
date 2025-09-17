87ef815a88ed3b2054d492d41ce1f7472e9534e0
23020797-DwanT <23020797@vnu.edu.vn>
2025-09-17 22:24:30 +0700

    1

---
+++ src/Solution.java
@@ -0,0 +1,11 @@
+public class Solution {
+    public static String printHello() {
+        return "HelloWorld";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(printHello());
+    }
+}
+
+
