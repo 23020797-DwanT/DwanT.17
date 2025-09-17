bb00a3d1c398dbabfae29d3bdef630b0d4aa011f
23020797-DwanT <23020797@vnu.edu.vn>
2025-09-17 22:24:30 +0700

    1

---
+++ src/Solution.java
@@ -0,0 +1,11 @@
+public class Solution {
+    public static String printHello() {
+        return "HelloWorld Phan Đăng Hải";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(printHello());
+    }
+}
+
+
