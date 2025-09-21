0dd836ab29ff4469af53843686d46349adabaf3e
23020797-DwanT <23020797@vnu.edu.vn>
2025-09-21 10:38:45 +0700

    Hello world

---
+++ OOPW1_04/src/Solution.java
@@ -0,0 +1,12 @@
+// Import your library
+// Do not change the name of the Solution class
+public class Solution {
+    public int gcd(int a, int b) {
+        while (b != 0) {
+            int t = b;
+            b = a % b;
+            a = t;
+        }
+        return a;
+    }
+}
