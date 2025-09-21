55cd2f2bce063a1f954e31a5f4645d6c1d9fe5ff
23020797-DwanT <23020797@vnu.edu.vn>
2025-09-21 10:52:06 +0700

    2

--- src/Solution.java
+++ src/Solution.java
@@ -1,7 +1,7 @@
 
 public class Solution {
     public static String printHello () {
-        System.out.println("HelloWorld Duong");
+        System.out.println("HelloWorld Duong gfdfg");
     }
     public static void main (String[] args) {
         Solution s = new Solution();
